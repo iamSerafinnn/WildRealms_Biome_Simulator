40adc20500d5bdf842c6746ce143a0dc90254546
Serafin O. Gargantiel III <gargantielserafin@gmail.com>
2026-07-07 15:20:04 -0500

    Fix base path for router and asset loading

--- CS 440 Group 4 Prototype/vite.config.js
+++ CS 440 Group 4 Prototype/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/440-Group-4-Fall-2025/',
+  base: '/WildRealms_Biome_Simulator/',
   plugins: [react()],
 })
